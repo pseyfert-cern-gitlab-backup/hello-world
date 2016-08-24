ef21e447a3ed1d227f169d40f5a2ada8188bce6c
Paul Seyfert <paul.seyfert@mib.infn.it>
2016-08-24 14:38:06 +0200

    USER needs to be set for lblogin

--- SetupEnv.sh
+++ SetupEnv.sh
@@ -1,2 +1,15 @@
 echo "setup"
+
+if [ -n "$USER" ]
+then
+  echo "hello $USER"
+else
+  export USER=nobody
+fi
+if [ -n "$HOME" ]
+then
+  echo "feel at home"
+else
+  export HOME=`pwd`
+fi
 source /cvmfs/lhcb.cern.ch/lib/lhcb/LBSCRIPTS/prod/InstallArea/scripts/LbLogin.sh -c x86_64-slc6-gcc49-opt
