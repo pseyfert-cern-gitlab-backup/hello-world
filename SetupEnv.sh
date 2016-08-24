5adcc7db0b975ac23809207ae4837d8cf223a64c
Paul Seyfert <paul.seyfert@mib.infn.it>
2016-08-24 14:40:25 +0200

    also setting up a project

--- SetupEnv.sh
+++ SetupEnv.sh
@@ -13,3 +13,6 @@ else
   export HOME=`pwd`
 fi
 source /cvmfs/lhcb.cern.ch/lib/lhcb/LBSCRIPTS/prod/InstallArea/scripts/LbLogin.sh -c x86_64-slc6-gcc49-opt
+
+#source /cvmfs/lhcb.cern.ch/lib/lhcb/LBSCRIPTS/LBSCRIPTS_v8r6p7/InstallArea/scripts/SetupProject.sh ROOT 6.06.02
+source /cvmfs/lhcb.cern.ch/lib/lhcb/LBSCRIPTS/LBSCRIPTS_v8r6p7/InstallArea/scripts/SetupProject.sh LCGCMT 84 ROOT
