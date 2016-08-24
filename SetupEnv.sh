f775be3c03735557f6357b83c26a101eb7a19ddc
Paul Seyfert <paul.seyfert@mib.infn.it>
2016-08-24 14:47:46 +0200

    randomly trying around

--- SetupEnv.sh
+++ SetupEnv.sh
@@ -15,4 +15,5 @@ fi
 source /cvmfs/lhcb.cern.ch/lib/lhcb/LBSCRIPTS/prod/InstallArea/scripts/LbLogin.sh -c x86_64-slc6-gcc49-opt
 
 #source /cvmfs/lhcb.cern.ch/lib/lhcb/LBSCRIPTS/LBSCRIPTS_v8r6p7/InstallArea/scripts/SetupProject.sh ROOT 6.06.02
-source /cvmfs/lhcb.cern.ch/lib/lhcb/LBSCRIPTS/LBSCRIPTS_v8r6p7/InstallArea/scripts/SetupProject.sh LCGCMT 84 ROOT
+#source /cvmfs/lhcb.cern.ch/lib/lhcb/LBSCRIPTS/LBSCRIPTS_v8r6p7/InstallArea/scripts/SetupProject.sh LCGCMT 84 ROOT
+source /cvmfs/lhcb.cern.ch/lib/lhcb/LBSCRIPTS/LBSCRIPTS_v8r6p7/InstallArea/scripts/SetupProject.sh DaVinci v41r2
