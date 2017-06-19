913bdcc8757d5b9f59ccdefd1cebe72b981f9242
Paul Seyfert <pseyfert@cern.ch>
2017-06-19 11:02:55 +0200

    more tests w/ compilation and stuff

--- main.cc
+++ main.cc
@@ -1,10 +1,11 @@
 #include "TCanvas.h"
 #include "TH1F.h"
 
-void mini() {
+int main() {
   TH1F* h = new TH1F("h","h",20,-5.,5.);
   h->FillRandom("gaus",300);
   TCanvas* c = new TCanvas();
   h->Draw();
   c->SaveAs("foobar.pdf");
+  return 0;
 }
